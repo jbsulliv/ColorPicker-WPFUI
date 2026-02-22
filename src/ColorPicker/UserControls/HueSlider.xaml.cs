127298ec2eb73b4413679297a58c69921fadb055
RealMeddsam <realferidjelalia@gmail.com>
2026-02-22 20:30:40 +0100

    Vertical Hue Slider

--- src/ColorPicker/UserControls/HueSlider.xaml.cs
+++ src/ColorPicker/UserControls/HueSlider.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Shapes;
 using ColorPicker.Models;
 
 namespace ColorPicker.UserControls
@@ -32,43 +30,6 @@ namespace ColorPicker.UserControls
         {
             get => (double)GetValue(SmallChangeProperty);
             set => SetValue(SmallChangeProperty, value);
-        }
-
-        private void OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            ((UIElement)sender).CaptureMouse();
-            var circle = (Path)sender;
-            var mousePos = e.GetPosition(circle);
-            UpdateValue(mousePos, circle.ActualWidth, circle.ActualHeight);
-        }
-
-        private void OnMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            ((UIElement)sender).ReleaseMouseCapture();
-        }
-
-        private void OnMouseMove(object sender, MouseEventArgs e)
-        {
-            if (!((UIElement)sender).IsMouseCaptured)
-                return;
-            var circle = (Path)sender;
-            var mousePos = e.GetPosition(circle);
-            UpdateValue(mousePos, circle.ActualWidth, circle.ActualHeight);
-        }
-
-        private void UpdateValue(Point mousePos, double width, double height)
-        {
-            var x = mousePos.X / (width * 2);
-            var y = mousePos.Y / (height * 2);
-
-            var length = Math.Sqrt(x * x + y * y);
-            if (length == 0)
-                return;
-            var angle = Math.Acos(x / length);
-            if (y < 0)
-                angle = -angle;
-            angle = angle * 360 / (Math.PI * 2) + 180;
-            Value = MathHelper.Clamp(angle, 0, 360);
         }
 
         private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs args)
